36d3d26b374fafea4c20d856ad7083942e28b2e7
AbrOuma <braham0369@gmail.com>
2023-03-16 23:47:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /*
 * main - Entry
+* Description: 'the program's description'
+* @parameter: describe the parameter
 * Return: Always 0
 */
 int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
 	return (0);
