912d9e7e560e0cafbe0033e55bb0fbc24fcb9e0d
AbrOuma <braham0369@gmail.com>
2023-03-23 00:37:32 -0700

    my chnages

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,30 @@
 #include "main.h"
 
 /**
- * _isalpha - function checking the aphabets
- *
- * @c: parameter being checked
- *
- * Return: 1 if it is an alphabet and 0 otherwisie
+ * times_table - print the 9 times table
  *
  */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 0; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
